10833c5860b6d45065151529ec930c274922ed6a
Ben Weaver <weaver@coptix.com>
2010-05-07 17:00:39 -0400

    Work out a few bugs; better logging in examples/client.js

--- examples/client.js
+++ examples/client.js
@@ -36,13 +36,46 @@ pre.withArgs(function(nick, host, port) {
         },
 
         MODE: function(msg, nick, mode) {
-            sys.puts('MODE changed for ' + nick + ' to ' + mode);
+            sys.log('MODE changed for ' + nick + ' to ' + mode);
         },
 
         PRIVMSG: function(msg, target, text) {
-            sys.puts('<' + msg.prefix + '> ' + text);
+            sys.log('<' + msg.nick + '> ' + text);
+        },
+
+        JOIN: function(msg, channel) {
+            activity(client, msg, 'joined ' + channel);
+        },
+
+        PART: function(msg, channel) {
+            activity(client, msg, 'left ' + channel);
+        },
+
+        QUIT: function(msg, reason) {
+            activity(client, msg, 'quit, ' + reason);
         }
     });
+
+    function activity(client, msg, action) {
+        var who = client.isMine(msg) ? 'You have ' : msg.nick + ' has ';
+        log(who + action);
+    }
+
+    function log(msg) {
+        sys.puts('[' + timestamp() + '] ' + msg);
+    }
+
+    function pad (n) {
+        return n < 10 ? '0' + n.toString(10) : n.toString(10);
+    }
+
+    function timestamp() {
+        var now = new Date();
+        return [
+            [now.getFullYear(), pad(now.getMonth()), pad(now.getDay())].join('.'),
+            [pad(now.getHours()), pad(now.getMinutes()), pad(now.getSeconds())].join(':')
+        ].join(' ');
+    }
 
     global.client = client;
     net.createServer(function(socket) {
